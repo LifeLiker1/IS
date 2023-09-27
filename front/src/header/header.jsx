61fddd8fc78914d972e464113d605183df9739ee
Ivan Nassonov <ivanych_54@mail.ru>
2023-09-27 22:48:07 +0600

    /

--- front/src/header/header.jsx
+++ front/src/header/header.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import "./header.scss";
-import { Button } from "antd";
+import { Button, Form, TreeSelect } from "antd";
+
+
 
 function Header() {
   return (
     <header>
       <nav>
-        
+        <Form.Item label="Мне нужны сотрудники">
+          <TreeSelect
+          treeData={[
+            {
+              title: "IT",
+              value: "IT",
+            },
+            {
+              title: "Технический отдел",
+              value: "Tech_Department",
+            },
+            {
+              title: "Юридический",
+              value: "HR",
+            },
+          ]}
+          />
+        </Form.Item>
         <Button href="/">На главную</Button>
         <Button href="/newEmployee">Добавить сотрудника</Button>
         <Button href="/login">Выход</Button>
