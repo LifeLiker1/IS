e34471f97fb2f8c35e933c17e30a2b1110047a58
Ivan Nassonov <ivanych_54@mail.ru>
2023-09-27 22:57:58 +0600

    added search field for HR

--- front/src/header/header.jsx
+++ front/src/header/header.jsx
@@ -2,30 +2,31 @@ import React from "react";
 import "./header.scss";
 import { Button, Form, TreeSelect } from "antd";
 
-
-
 function Header() {
   return (
     <header>
-      <nav>
-        <Form.Item label="Мне нужны сотрудники">
+      <div className="search_field">
+        <Form.Item label="Мне нужны сотрудники" className="dep_sel">
           <TreeSelect
-          treeData={[
-            {
-              title: "IT",
-              value: "IT",
-            },
-            {
-              title: "Технический отдел",
-              value: "Tech_Department",
-            },
-            {
-              title: "Юридический",
-              value: "HR",
-            },
-          ]}
+            treeData={[
+              {
+                title: "IT",
+                value: "IT",
+              },
+              {
+                title: "Технический отдел",
+                value: "Tech_Department",
+              },
+              {
+                title: "Юридический",
+                value: "HR",
+              },
+            ]}
           />
         </Form.Item>
+      </div>
+
+      <nav>
         <Button href="/">На главную</Button>
         <Button href="/newEmployee">Добавить сотрудника</Button>
         <Button href="/login">Выход</Button>
