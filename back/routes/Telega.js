d38ec813fd4bd20e1d7a84dbdb69e91a52d3905b
Ivan Nassonov <ivanych_54@mail.ru>
2023-10-30 21:20:53 +0600

    добавлена функция авторизации по номеру телефона в ТГ боте

--- back/routes/Telega.js
+++ back/routes/Telega.js
@@ -3,8 +3,79 @@ const TelegramBot = require("node-telegram-bot-api");
 const fetch = require("node-fetch");
 require("dotenv").config();
 
+const { commandsForUser } = require("./telegramFunctions");
 const router = Router();
 const bot = new TelegramBot(process.env.BOT_API, { polling: true });
+
+const commands = commandsForUser;
+let isAuth = false;
+
+bot.setMyCommands(commands);
+
+bot.onText(/\/start/, async (msg) => {
+  const chatId = msg.chat.id;
+  const options = {
+    reply_markup: {
+      keyboard: [
+        [
+          {
+            text: "Авторизация",
+            request_contact: true,
+          },
+        ],
+      ],
+      resize_keyboard: true,
+      one_time_keyboard: true,
+    },
+  };
+  bot.sendMessage(
+    chatId,
+    "Здравствуйте. Для авторизации нажмите кнопку 'Авторизация'.",
+    options
+  );
+});
+
+bot.on("contact", (msg) => {
+  const chatId = msg.chat.id;
+  const phoneNumber = msg.contact.phone_number;
+  async function SearchPhone() {
+    const response = await fetch("http://localhost:3001/api/employees", {
+      method: "GET",
+    });
+    try {
+      const result = await response.json();
+      const findEmployee = result.find((employee) => employee.mobilePhone === phoneNumber)
+      if(findEmployee){
+        const isAuth = true
+        bot.sendMessage(chatId, `Здравствйте ${findEmployee.name} ${findEmployee.surname}`);
+      }else{
+        const isAuth = false
+        bot.sendMessage(chatId, `Пользователя с таким номером телефона не найдено`);
+      }
+      console.log(phoneNumber)
+      console.log(findEmployee)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  SearchPhone();
+  bot.sendMessage(chatId, `Вы ввели номер телефона: ${phoneNumber}`);
+  // Здесь можно добавить код для дальнейших действий, связанных с авторизацией.
+});
+
+
+// async function Auth(chatId) {
+//   try {
+//     const response = await fetch("http://localhost:3001/api/employees");
+//     if (response.ok) {
+//       console.log("Hello World");
+//     }
+//     const result = await response.json();
+//     const findEmployee = result.findOne({ mobilePhone: "77471082141" });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
 async function Employee(chatId) {
   try {
@@ -35,13 +106,13 @@ async function Employee(chatId) {
 
 async function SetTicket(chatId) {
   try {
-    const response = await fetch("http://localhost:3001/api/tickets/decrease",{
+    const response = await fetch("http://localhost:3001/api/tickets/decrease", {
       method: "POST",
     });
     if (!response.ok) {
       throw new Error("Ошибка при выполнении запроса");
-    }else{
-      bot.sendMessage(chatId, "Талоны загружены")
+    } else {
+      bot.sendMessage(chatId, "Талоны загружены");
     }
   } catch (error) {
     console.error("Произошла ошибка:", error);
@@ -56,7 +127,6 @@ async function Application(chatId) {
     }
     const application = await response.json();
 
-    // Преобразуйте данные о сотрудниках в нужный формат
     const applicationText = application
       .map((application, index) => {
         return `Заявка ${index + 1}\nМодель: ${
@@ -80,32 +150,9 @@ async function Application(chatId) {
 }
 
 
-const commands = [
-  {
-    command: "start",
-    description: "Запуск бота",
-  },
-  {
-    command: "employee",
-    description: "Получить всех сотрудников",
-  },
-  {
-    command: "application",
-    description: "Все заявки",
-  },
-  { command: "tickets", description: "Установил талон" },
-];
-
-bot.setMyCommands(commands);
-
-bot.onText(/\/start/, async (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Здравствуйте.");
-});
-
 bot.onText(/\/employee/, async (msg) => {
   const chatId = msg.chat.id;
-  try {
+  if(!isAuth){try {
     await Employee(chatId);
   } catch (error) {
     bot.sendMessage(
@@ -113,7 +160,10 @@ bot.onText(/\/employee/, async (msg) => {
       "Произошла ошибка при получении данных о сотрудниках."
     );
     console.error("Произошла ошибка:", error);
+  }}else{
+    bot.sendMessage(chatId, "Вы не авторизированы")
   }
+  
 });
 bot.onText(/\/application/, async (msg) => {
   const chatId = msg.chat.id;
@@ -125,7 +175,7 @@ bot.onText(/\/application/, async (msg) => {
   }
 });
 
-bot.onText(/\/tickets/, async(msg) => {
+bot.onText(/\/tickets/, async (msg) => {
   const chatId = msg.chat.id;
   try {
     await SetTicket(chatId);
@@ -133,6 +183,16 @@ bot.onText(/\/tickets/, async(msg) => {
     bot.sendMessage(chatId, "Произошла ошибка при получении данных");
     console.error("Произошла ошибка:", error);
   }
-})
+});
+
+bot.onText(/\/auth/, async (msg) => {
+  const chatId = msg.chat.id;
+  try {
+    await Auth(chatId);
+  } catch (error) {
+    bot.sendMessage(chatId, "Произошла ошибка при получении данных");
+    console.error("Произошла ошибка:", error);
+  }
+});
 
 module.exports = router;
